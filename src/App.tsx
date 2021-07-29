4035d8478b2532f655b7d03484fbace2785ed09f
Benjamin Helstrip <ben.helstrip@cadju.com>
2021-07-29 17:12:56 +0100

    init react app with typescript. add simple landing page graphic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import useWindowSize from "@rooks/use-window-size";
+import ParticleImage, {
+  ParticleOptions,
+  Vector,
+  forces,
+  ParticleForce
+} from "react-particle-image";
+ 
+const particleOptions : ParticleOptions = {
+  filter: ({ x, y, image }) => {
+    // Get pixel
+    const pixel = image.get(x, y);
+    // Make a particle for this pixel if blue > 50 (range 0-255)
+    return pixel.b > 50;
+  },
+  color: ({ x, y, image }) => "#a4886a",
+  radius: () => Math.random() * 1.5 + 0.5,
+  mass: () => 20,
+  friction: () => 0.15,
+  initialPosition: ({ canvasDimensions }) => {
+    return new Vector(canvasDimensions.width / 2, canvasDimensions.height / 2);
+  }
+};
+ 
+const motionForce = (x: any, y: any) => {
+  return forces.disturbance(x, y, 15);
+};
+ 
 function App() {
+  const { innerWidth, innerHeight } = useWindowSize();
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ParticleImage
+      src={"/0xv002-comingsoon.png"}
+      width={Number(innerWidth)}
+      height={Number(innerHeight)}
+      scale={0.75}
+      entropy={5}
+      maxParticles={12000}
+      particleOptions={particleOptions}
+      mouseMoveForce={motionForce}
+      touchMoveForce={motionForce}
+      backgroundColor="#191D1F"
+    />
   );
 }
 
