1c259b0c3c820fb1aa6a5f2295279bbb7f6481b9
Framsynt <virtuousno1@protonmail.com>
2021-11-09 23:27:14 +0100

    add membership form

--- src/js/email.js
+++ src/js/email.js
@@ -2,61 +2,72 @@ import anime, { timeline } from "animejs";
 
 export function emailApplication() {
   const formWrapper = document.querySelector(".form-wrapper");
-  const form = document.querySelector(".js-contact-form");
-  const applyButton = document.querySelector(".js-apply");
-  const showFormButton = document.querySelector(".js-show-form");
-  const alert = document.querySelector(".js-alert");
-  const formFields = form.elements;
+  const forms = {
+    funding: document.querySelector(".js-funding-form"),
+    member: document.querySelector(".js-member-form"),
+  };
+  const applyButtons = {
+    funding: document.querySelector(".js-apply-funding"),
+    member: document.querySelector(".js-apply-member"),
+  };
+  const showFormFundingButton = document.querySelector(".js-show-form");
+  const showFormMemberButton = document.querySelector(".js-show-form-member");
   var canSend = true;
-  var formIsOpen = false;
   var validate = require("validate.js");
+  var openForm = null;
 
-  applyButton.addEventListener("click", (event) => {
-    resolveClick(event);
+  applyButtons.funding.addEventListener("click", (event) => {
+    sendEmail(event, forms.funding);
   });
 
-  showFormButton.addEventListener("click", (event) => {
-    showForm(event);
+  applyButtons.member.addEventListener("click", (event) => {
+    sendEmail(event, forms.member);
   });
 
-  function showForm() {
-    if (formIsOpen) {
+  showFormFundingButton.addEventListener("click", (event) => {
+    toggleForm(event, forms.funding);
+  });
+  showFormMemberButton.addEventListener("click", (event) => {
+    toggleForm(event, forms.member);
+  });
+
+  //
+  //
+  //
+  function toggleForm(event, form) {
+    if (openForm === form) {
       return;
     }
 
-
-    var hideButtonAnime = anime({
-      easing: "easeOutCubic",
-      duration: 1000,
-      targets: showFormButton.parentNode,
-      opacity: [1, 0],
-      height: 0,
-      complete() {
-        formIsOpen = true;
-        showFormButton.classList.add("hidden");
-        form["email"].focus();
-      },
-    });
+    form.style.display = "block";
 
     var showFormAnime = anime.timeline({
       easing: "easeInCubic",
       duration: 600,
     });
 
-    form.classList.remove("contact-form--hidden");
-
     showFormAnime
       .add({
         targets: formWrapper,
         height: form.clientHeight,
-        complete() {
-          formWrapper.style.height = "auto";
-        },
       })
       .add(
         {
           targets: form,
           opacity: [0, 1],
+        },
+        0
+      )
+      .add(
+        {
+          targets: openForm,
+          opacity: [1, 0],
+          complete: function (anim) {
+            if (openForm !== null) {
+              openForm.style.display = "none";
+            }
+            openForm = form;
+          },
         },
         0
       )
@@ -69,8 +80,171 @@ export function emailApplication() {
         },
         0
       );
+
+    console.log(openForm);
   }
 
+  //
+  //
+  //
+  function sendEmail(event, form) {
+    const checkOk = checkForm(form);
+    const alert = form.querySelector(".js-alert");
+    const formFields = form.elements;
+    const emailTemplate = "template_q2ibls3";
+    var templateParams;
+
+    if (!canSend) {
+      return;
+    }
+
+    if (!checkOk) {
+      alert.innerHTML = templateParams.alert;
+      alert.classList.add("form-alert--visible", "form-alert--wrong");
+      return;
+    }
+
+    if (form === forms.funding) {
+      templateParams = {
+        email: formFields["email"].value.trim(),
+        projectName: formFields["projectName"].value.trim(),
+        website: formFields["website"].value.trim(),
+        twitter: formFields["twitter"].value.trim(),
+        nameContact: formFields["nameContact"].value.trim(),
+        contactPosition: formFields["contactPosition"].value.trim(),
+        description: formFields["description"].value.trim(),
+        alert: "Please fill out all fields.",
+      };
+      sendEmailFunding(templateParams, applyButtons.funding, alert);
+    } else {
+      templateParams = {
+        emailMember: formFields["emailMember"].value.trim(),
+        twitterMember: formFields["twitterMember"].value.trim(),
+        telegramMember: formFields["telegramMember"].value.trim(),
+        discordMember: formFields["discordMember"].value.trim(),
+        sinceWhenMember: formFields["sinceWhenMember"].value.trim(),
+        defiProtocolsMember: formFields["defiProtocolsMember"].value.trim(),
+        capitalMember: formFields["capitalMember"].value.trim(),
+        farmingMember: getCheckBoxesValue(form, "farmingMember").trim(),
+        walletMember: formFields["walletMember"].value.trim(),
+        assetsMember: getCheckBoxesValue(form, "assetsMember").trim(),
+        yieldMember: getCheckBoxesValue(form, "yieldMember").trim(),
+        degenRatioMember: formFields["degenRatioMember"].value.trim(),
+        degenScoreMember: formFields["degenScoreMember"].value.trim(),
+        earlyMember: formFields["earlyMember"].value.trim(),
+        recentInvestmentsMember:
+          formFields["recentInvestmentsMember"].value.trim(),
+        rugpullMember: formFields["rugpullMember"].value.trim(),
+        publishedMember: formFields["publishedMember"].value.trim(),
+        valueMember: formFields["valueMember"].value.trim(),
+        elseMember: formFields["elseMember"].value.trim(),
+        alert: "Please fill out your contact information.",
+      };
+      sendEmailMember(templateParams, applyButtons.member, alert);
+    }
+
+
+
+    // stop button
+    canSend = false;
+
+  }
+
+  function getCheckBoxesValue(form, group) {
+    const formFields = form.elements;
+    const checkboxes = formFields[group];
+
+    var r = "";
+
+    checkboxes.forEach((element) => {
+      if (element.checked === true) {
+        r += element.value + " | ";
+      }
+    });
+
+
+    return r;
+  }
+
+  function sendEmailFunding(templateParams, applyButton, alert) {
+    applyButton.classList.add("form-button--loading");
+    emailjs.send("default_service", "template_nrbw9wi", templateParams).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        alert.innerHTML = "Thank you. Your application is on its way.";
+        if (alert.classList.contains("form-alert--wrong")) {
+          alert.classList.remove("form-alert--wrong");
+        } else {
+          alert.classList.add("form-alert--thanks", "form-alert--visible");
+          var showAlert = anime({
+            easing: "easeInCubic",
+            duration: 1000,
+            targets: alert,
+            opacity: [0, 1],
+          });
+        }
+
+        var hideButtonAnime = anime({
+          easing: "easeOutCubic",
+          duration: 1000,
+          targets: applyButton.parentNode,
+          opacity: [1, 0],
+          height: 0,
+          margin: 0,
+          complete() {
+            applyButton.classList.add("hidden");
+          },
+        });
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        canSend = true;
+        applyButton.classList.remove("form-button--loading");
+      }
+    );
+  }
+
+  function sendEmailMember(templateParams, applyButton, alert) {
+    applyButton.classList.add("form-button--loading");
+    emailjs.send("default_service", "template_q2ibls3", templateParams).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        alert.innerHTML = "Thank you. Your application is on its way.";
+        if (alert.classList.contains("form-alert--wrong")) {
+          alert.classList.remove("form-alert--wrong");
+        } else {
+          alert.classList.add("form-alert--thanks", "form-alert--visible");
+          var showAlert = anime({
+            easing: "easeInCubic",
+            duration: 1000,
+            targets: alert,
+            opacity: [0, 1],
+          });
+        }
+
+        var hideButtonAnime = anime({
+          easing: "easeOutCubic",
+          duration: 1000,
+          targets: applyButton.parentNode,
+          opacity: [1, 0],
+          height: 0,
+          margin: 0,
+          complete() {
+            applyButton.classList.add("hidden");
+          },
+        });
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        canSend = true;
+        applyButton.classList.remove("form-button--loading");
+      }
+    );
+  }
+
+  //
+  // old
+  //
   function resolveClick(event) {
     var checkOk = checkForm();
 
@@ -85,15 +259,7 @@ export function emailApplication() {
     }
 
     // gather message
-    var templateParams = {
-      email: formFields["email"].value.trim(),
-      projectName: formFields["projectName"].value.trim(),
-      website: formFields["website"].value.trim(),
-      twitter: formFields["twitter"].value.trim(),
-      nameContact: formFields["nameContact"].value.trim(),
-      contactPosition: formFields["contactPosition"].value.trim(),
-      description: formFields["description"].value.trim(),
-    };
+    var templateParams = {};
 
     // stop button
     canSend = false;
@@ -135,34 +301,11 @@ export function emailApplication() {
     );
   }
 
-  function checkForm() {
-    var constraints = {
-      email: {
-        // If email is required
-        presence: true,
-        // and you want to check if this is an email
-        email: true,
-      },
-      projectName: {
-        presence: true,
-      },
-      website: {
-        presence: true,
-      },
-      twitter: {
-        presence: true,
-      },
-      nameContact: {
-        presence: true,
-      },
-      contactPosition: {
-        presence: true,
-      },
-      description: {
-        presence: true,
-      },
-    };
-    var errors = validate(form, constraints);
+  function checkForm(form) {
+    const constraints =
+      form === forms.funding ? fundingConstrains : memberConstrains;
+
+    const errors = validate(form, constraints);
 
     if (!errors) {
       return true;
@@ -170,4 +313,35 @@ export function emailApplication() {
 
     return false;
   }
+
+  const fundingConstrains = {
+    email: {
+      // If email is required
+      presence: true,
+      // and you want to check if this is an email
+      email: true,
+    },
+    projectName: {
+      presence: true,
+    },
+    website: {
+      presence: true,
+    },
+    twitter: {
+      presence: true,
+    },
+    nameContact: {
+      presence: true,
+    },
+    contactPosition: {
+      presence: true,
+    },
+    description: {
+      presence: true,
+    },
+  };
+
+  const memberConstrains = {
+
+  };
 }
